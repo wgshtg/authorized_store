06031f83ef8969bfcd2f2d4b74c0b2464dac403d
kevin_kj_peng <kevin_kj_peng@gss.com.tw>
2021-04-02 14:45:37 +0800

    feat: add web app project and stores api endpoint

---
+++ src/WebApp/Controllers/StoresController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using AuthorizedStore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StoresController : ControllerBase
+    {
+        private readonly IStoreDao _storeDao;
+
+        public StoresController(IStoreDao storeDao)
+        {
+            _storeDao = storeDao;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var store = await _storeDao.GetAsync(id);
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(store);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStoresAsync([FromQuery]SearchCriteria criteria)
+        {
+            if (criteria == null)
+            {
+                return BadRequest();
+            }
+
+            var stores = await _storeDao.FindAllAsync(criteria);
+
+            return Ok(stores);
+        }
+    }
+}
